003dceb8b5354fd05371054880e37e7f31bc1ff5
Acheniche <99502683+Acheniche@users.noreply.github.com>
2023-04-11 19:48:50 +0300

    feat: update file

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,17 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  if (typeof (sampleActivity) !== 'string')
+    return false;
+  if (isNaN(+sampleActivity))
+    return false
+  else {
+    let years = Math.ceil((HALF_LIFE_PERIOD * Math.log(MODERN_ACTIVITY / (+sampleActivity))) / 0.693);
+    if (isNaN(years) || years < 0 || years == Infinity || years == -Infinity)
+      return false;
+    return years;
+  }
 }
 
 module.exports = {
